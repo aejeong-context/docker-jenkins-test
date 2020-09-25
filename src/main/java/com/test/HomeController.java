0176015fbee5737d7f8de67e923e4c2b5c417368
aejeong <aejeong-context@kakao.com>
2020-09-25 14:17:35 +0900

    제발

--- src/main/java/com/test/HomeController.java
+++ src/main/java/com/test/HomeController.java
@@ -14,6 +14,6 @@ public class HomeController {
     @GetMapping("/aejeong")
     public String ae(){
         System.out.println("gggg");
-        return "ㅎㅎㅎㅎㅎㅎㅎqqqㅎㅎㅎㅎ";
+        return "ㅎㅎㅎㅎㅎㅎㅎqqqㅎㅎㅎㅎaz";
     }
 }
